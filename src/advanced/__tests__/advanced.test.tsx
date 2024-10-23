657a49ab9d0f82b0cf9a0e070061b946abf0c5a0
lsw <hkbvc1222@gmail.com>
2024-10-24 02:04:05 +0900

    test: mock api 테스트 케이스 추가

--- src/advanced/__tests__/advanced.test.tsx
+++ src/advanced/__tests__/advanced.test.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { describe, expect, test } from 'vitest';
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
 import {
   act,
   fireEvent,
   render,
   renderHook,
   screen,
+  waitFor,
   within,
 } from '@testing-library/react';
 import { CartPage } from '../../refactoring/components/CartPage';
@@ -17,7 +18,13 @@ import {
   getApplyCouponPrice,
 } from '@refactor/hooks/utils/couponUtil';
 import { convertPercentage } from '@refactor/hooks/utils/discountUtil';
-import { useNewProductForm } from '@refactor/hooks/useNewProductForm';
+import { useProductForm } from '@refactor/hooks/useProductForm';
+import { useDiscountForm } from '@refactor/hooks/useDiscountFrom';
+import { useCouponForm } from '@refactor/hooks/useCouponForm';
+import { useStock } from '@refactor/hooks/useStock';
+import { useApiMockProduct } from '@refactor/hooks/useApiMockProduct';
+import { useApiMockCoupon } from '@refactor/hooks/useApiMockCoupon';
+import { initialProducts } from '@refactor/data/item';
 
 const mockProducts: Product[] = [
   {
@@ -74,6 +81,28 @@ const TestAdminPage = () => {
   const handleCouponAdd = (newCoupon: Coupon) => {
     setCoupons((prevCoupons) => [...prevCoupons, newCoupon]);
   };
+
+  return (
+    <AdminPage
+      products={products}
+      coupons={coupons}
+      onProductUpdate={handleProductUpdate}
+      onProductAdd={handleProductAdd}
+      onCouponAdd={handleCouponAdd}
+    />
+  );
+};
+
+const TestApiMockAdminPage = () => {
+  const { products, updateProduct, addProduct } = useApiMockProduct();
+  const { coupons, addCoupon } = useApiMockCoupon();
+
+  const handleProductUpdate = (updatedProduct: Product) =>
+    updateProduct(updatedProduct);
+
+  const handleProductAdd = (newProduct: Product) => addProduct(newProduct);
+
+  const handleCouponAdd = (newCoupon: Coupon) => addCoupon(newCoupon);
 
   return (
     <AdminPage
@@ -275,6 +304,136 @@ describe('advanced > ', () => {
     });
   });
 
+  describe('[api-mock] ', () => {
+    test('관리자 페이지 테스트 >', async () => {
+      render(<TestApiMockAdminPage />);
+      await waitFor(() =>
+        expect(screen.getByTestId('product-1')).toBeInTheDocument()
+      );
+      const $product1 = screen.getByTestId('product-1');
+      // 1. 새로운 상품 추가
+      fireEvent.click(screen.getByText('새 상품 추가'));
+      fireEvent.change(screen.getByLabelText('상품명'), {
+        target: { value: '상품4' },
+      });
+      fireEvent.change(screen.getByLabelText('가격'), {
+        target: { value: '15000' },
+      });
+      fireEvent.change(screen.getByLabelText('재고'), {
+        target: { value: '30' },
+      });
+      fireEvent.click(screen.getByText('추가'));
+
+      await waitFor(() =>
+        expect(screen.getByTestId('product-4')).toBeInTheDocument()
+      );
+      const $product4 = screen.getByTestId('product-4');
+      expect($product4).toHaveTextContent('상품4');
+      expect($product4).toHaveTextContent('15000원');
+      expect($product4).toHaveTextContent('재고: 30');
+      // 2. 상품 선택 및 수정
+      fireEvent.click($product1);
+      fireEvent.click(within($product1).getByTestId('toggle-button'));
+      fireEvent.click(within($product1).getByTestId('modify-button'));
+      act(() => {
+        fireEvent.change(within($product1).getByDisplayValue('20'), {
+          target: { value: '25' },
+        });
+        fireEvent.change(within($product1).getByDisplayValue('10000'), {
+          target: { value: '12000' },
+        });
+        fireEvent.change(within($product1).getByDisplayValue('상품1'), {
+          target: { value: '수정된 상품1' },
+        });
+      });
+
+      fireEvent.click(within($product1).getByText('수정 완료'));
+
+      await waitFor(() => expect($product1).toHaveTextContent('수정된 상품1'));
+      expect($product1).toHaveTextContent('12000원');
+      expect($product1).toHaveTextContent('재고: 25');
+      // 3. 상품 할인율 추가 및 삭제
+      fireEvent.click($product1);
+      fireEvent.click(within($product1).getByTestId('modify-button'));
+      // 할인 추가
+      act(() => {
+        fireEvent.change(screen.getByPlaceholderText('수량'), {
+          target: { value: '5' },
+        });
+        fireEvent.change(screen.getByPlaceholderText('할인율 (%)'), {
+          target: { value: '5' },
+        });
+      });
+      fireEvent.click(screen.getByText('할인 추가'));
+      expect(
+        screen.queryByText('5개 이상 구매 시 5% 할인')
+      ).toBeInTheDocument();
+
+      // 할인 삭제
+      fireEvent.click(screen.getAllByText('삭제')[0]);
+
+      // 삭제 확인
+      expect(
+        screen.queryByText('10개 이상 구매 시 10% 할인')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('20개 이상 구매 시 20% 할인')
+      ).toBeInTheDocument();
+      expect(
+        screen.queryByText('5개 이상 구매 시 5% 할인')
+      ).toBeInTheDocument();
+
+      // 할인 삭제
+      fireEvent.click(screen.getAllByText('삭제')[0]);
+
+      // 삭제 확인
+      expect(
+        screen.queryByText('10개 이상 구매 시 10% 할인')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('20개 이상 구매 시 20% 할인')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('5개 이상 구매 시 5% 할인')
+      ).toBeInTheDocument();
+
+      // 할인 삭제
+      fireEvent.click(screen.getAllByText('삭제')[0]);
+
+      // 삭제 확인
+      expect(
+        screen.queryByText('10개 이상 구매 시 10% 할인')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('20개 이상 구매 시 20% 할인')
+      ).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('5개 이상 구매 시 5% 할인')
+      ).not.toBeInTheDocument();
+
+      // 4. 쿠폰 추가
+      fireEvent.change(screen.getByPlaceholderText('쿠폰 이름'), {
+        target: { value: '새 쿠폰' },
+      });
+      fireEvent.change(screen.getByPlaceholderText('쿠폰 코드'), {
+        target: { value: 'NEW10' },
+      });
+      fireEvent.change(screen.getByRole('combobox'), {
+        target: { value: 'percentage' },
+      });
+      fireEvent.change(screen.getByPlaceholderText('할인 값'), {
+        target: { value: '10' },
+      });
+      fireEvent.click(screen.getByText('쿠폰 추가'));
+
+      await waitFor(() =>
+        expect(screen.getByTestId('coupon-3')).toBeInTheDocument()
+      );
+      const $newCoupon = screen.getByTestId('coupon-3');
+      expect($newCoupon).toHaveTextContent('새 쿠폰 (NEW10):10% 할인');
+    });
+  });
+
   describe('[utils] 함수 테스트', () => {
     test('formatCouponOptions - 콤보박스 생성', () => {
       // 5000원 할인 쿠폰일 경우
@@ -332,70 +491,354 @@ describe('advanced > ', () => {
     });
   });
 
-  describe('[hooks] useNewProductForm', () => {
+  describe('[hooks]', () => {
     const initialNewProductForm = {
       name: '',
       price: 0,
       stock: 0,
       discounts: [],
     };
-    test('[제품 추가 Form]: 초기값 설정 기능', () => {
-      const { result } = renderHook(() =>
-        useNewProductForm({ ...initialNewProductForm })
-      );
+    test('useApiMockCoupon: 쿠폰 mock api', async () => {
+      const { result } = renderHook(() => useApiMockCoupon());
+
+      // coupons 상태가 비어있는지 확인
+      expect(result.current.coupons).toEqual([]);
+
+      // 비동기적으로 데이터가 로드될 때까지 대기
+      await waitFor(() => {
+        expect(result.current.coupons).not.toEqual([]);
+      });
+
       // 초기화 값 확인
-      expect(result.current.newProductForm.name).toBe('');
-      expect(result.current.newProductForm.price).toBe(0);
-      expect(result.current.newProductForm.stock).toBe(0);
+      expect(result.current.coupons).toEqual([
+        {
+          name: '5000원 할인 쿠폰',
+          code: 'AMOUNT5000',
+          discountType: 'amount',
+          discountValue: 5000,
+        },
+        {
+          name: '10% 할인 쿠폰',
+          code: 'PERCENT10',
+          discountType: 'percentage',
+          discountValue: 10,
+        },
+      ]);
+
+      // 쿠폰 추가
+      act(() =>
+        result.current.addCoupon({
+          name: '새 쿠폰',
+          code: 'NEW10',
+          discountType: 'percentage',
+          discountValue: 10,
+        })
+      );
+
+      // 비동기
+      await waitFor(() =>
+        expect(result.current.coupons).not.toEqual([
+          {
+            name: '5000원 할인 쿠폰',
+            code: 'AMOUNT5000',
+            discountType: 'amount',
+            discountValue: 5000,
+          },
+          {
+            name: '10% 할인 쿠폰',
+            code: 'PERCENT10',
+            discountType: 'percentage',
+            discountValue: 10,
+          },
+        ])
+      );
+
+      // 추가된 쿠폰 확인
+      expect(result.current.coupons).toEqual([
+        {
+          name: '5000원 할인 쿠폰',
+          code: 'AMOUNT5000',
+          discountType: 'amount',
+          discountValue: 5000,
+        },
+        {
+          name: '10% 할인 쿠폰',
+          code: 'PERCENT10',
+          discountType: 'percentage',
+          discountValue: 10,
+        },
+        {
+          name: '새 쿠폰',
+          code: 'NEW10',
+          discountType: 'percentage',
+          discountValue: 10,
+        },
+      ]);
+
+      // 쿠폰 제거
+      act(() =>
+        result.current.removeCoupon({
+          name: '새 쿠폰',
+          code: 'NEW10',
+          discountType: 'percentage',
+          discountValue: 10,
+        })
+      );
+
+      // 비동기
+      await waitFor(() =>
+        expect(result.current.coupons).not.toEqual([
+          {
+            name: '5000원 할인 쿠폰',
+            code: 'AMOUNT5000',
+            discountType: 'amount',
+            discountValue: 5000,
+          },
+          {
+            name: '10% 할인 쿠폰',
+            code: 'PERCENT10',
+            discountType: 'percentage',
+            discountValue: 10,
+          },
+          {
+            name: '새 쿠폰',
+            code: 'NEW10',
+            discountType: 'percentage',
+            discountValue: 10,
+          },
+        ])
+      );
+
+      // 제거된 쿠폰 확인
+      expect(result.current.coupons).toEqual([
+        {
+          name: '5000원 할인 쿠폰',
+          code: 'AMOUNT5000',
+          discountType: 'amount',
+          discountValue: 5000,
+        },
+        {
+          name: '10% 할인 쿠폰',
+          code: 'PERCENT10',
+          discountType: 'percentage',
+          discountValue: 10,
+        },
+      ]);
     });
 
-    test('[제품 추가 Form]: 값 업데이트 기능', () => {
-      const { result } = renderHook(() =>
-        useNewProductForm({ ...initialNewProductForm })
+    test('useApiMockProduct: 제품 mock api', async () => {
+      const { result } = renderHook(() => useApiMockProduct());
+
+      // products 상태가 비어있는지 확인
+      expect(result.current.products).toEqual([]);
+
+      // 비동기적으로 데이터가 로드될 때까지 대기
+      await waitFor(() => {
+        expect(result.current.products).not.toEqual([]);
+      });
+
+      // 초기화 값 확인
+      expect(result.current.products).toEqual(initialProducts);
+
+      // 제품 추가
+      result.current.addProduct({
+        id: 'p4',
+        name: '상품4',
+        price: 40000,
+        stock: 40,
+        discounts: [{ quantity: 20, rate: 0.15 }],
+      });
+
+      // 비동기
+      await waitFor(() =>
+        expect(result.current.products).not.toEqual(initialProducts)
       );
-      expect(result.current.newProductForm.name).toBe('');
-      expect(result.current.newProductForm.price).toBe(0);
-      expect(result.current.newProductForm.stock).toBe(0);
 
-      // name 업데이트 확인
-      act(() => result.current.updateNewProductForm('name', '제품1'));
-      expect(result.current.newProductForm.name).toBe('제품1');
+      // 추가된 제품 확인
+      expect(result.current.products).toEqual([
+        ...initialProducts,
+        {
+          id: 'p4',
+          name: '상품4',
+          price: 40000,
+          stock: 40,
+          discounts: [{ quantity: 20, rate: 0.15 }],
+        },
+      ]);
 
-      // price 업데이트 확인
-      act(() => result.current.updateNewProductForm('price', 10000));
-      expect(result.current.newProductForm.price).toBe(10000);
+      // 제품 제거
+      act(() => result.current.removeProduct('p4'));
 
-      // stock 업데이트 확인
-      act(() => result.current.updateNewProductForm('stock', 20));
-      expect(result.current.newProductForm.stock).toBe(20);
+      // 비동기
+      await waitFor(() =>
+        expect(result.current.products).not.toEqual([
+          ...initialProducts,
+          {
+            id: 'p4',
+            name: '상품4',
+            price: 40000,
+            stock: 40,
+            discounts: [{ quantity: 20, rate: 0.15 }],
+          },
+        ])
+      );
+
+      // 제거된 제품 확인
+      expect(result.current.products).toEqual(initialProducts);
     });
 
-    test('[제품 추가 Form]: 리셋 기능', () => {
+    test('useProductForm: 제품추가 form', () => {
       const { result } = renderHook(() =>
-        useNewProductForm({ ...initialNewProductForm })
+        useProductForm({ ...initialNewProductForm })
       );
-      expect(result.current.newProductForm.name).toBe('');
-      expect(result.current.newProductForm.price).toBe(0);
-      expect(result.current.newProductForm.stock).toBe(0);
+      expect(result.current.productForm.name).toBe('');
+      expect(result.current.productForm.price).toBe(0);
+      expect(result.current.productForm.stock).toBe(0);
 
       // name 업데이트 확인
-      act(() => result.current.updateNewProductForm('name', '제품1'));
-      expect(result.current.newProductForm.name).toBe('제품1');
+      act(() => result.current.updateProductForm('name', '제품1'));
+      expect(result.current.productForm.name).toBe('제품1');
 
       // price 업데이트 확인
-      act(() => result.current.updateNewProductForm('price', 10000));
-      expect(result.current.newProductForm.price).toBe(10000);
+      act(() => result.current.updateProductForm('price', 10000));
+      expect(result.current.productForm.price).toBe(10000);
 
       // stock 업데이트 확인
-      act(() => result.current.updateNewProductForm('stock', 20));
-      expect(result.current.newProductForm.stock).toBe(20);
+      act(() => result.current.updateProductForm('stock', 20));
+      expect(result.current.productForm.stock).toBe(20);
+
+      // discounts add 확인
+      act(() => result.current.addDiscount({ quantity: 10, rate: 20 }));
+      act(() => result.current.addDiscount({ quantity: 20, rate: 40 }));
+      expect(result.current.productForm.discounts).toEqual([
+        { quantity: 10, rate: 20 },
+        { quantity: 20, rate: 40 },
+      ]);
+
+      // discounts remove 확인
+      act(() => result.current.removeDiscount(1));
+      expect(result.current.productForm.discounts).toEqual([
+        { quantity: 10, rate: 20 },
+      ]);
 
       // 값 초기화 확인
-      act(() => result.current.resetNewProductForm());
-      expect(result.current.newProductForm.name).toBe('');
-      expect(result.current.newProductForm.price).toBe(0);
-      expect(result.current.newProductForm.stock).toBe(0);
-      expect(result.current.newProductForm.discounts).toEqual([]);
+      act(() => result.current.resetProductForm());
+      expect(result.current.productForm.name).toBe('');
+      expect(result.current.productForm.price).toBe(0);
+      expect(result.current.productForm.stock).toBe(0);
+      expect(result.current.productForm.discounts).toEqual([]);
+
+      // 값 세팅 확인
+      act(() =>
+        result.current.setProductForm({
+          name: '제품2',
+          price: 20000,
+          stock: 104,
+          discounts: [{ quantity: 50, rate: 0.4 }],
+        })
+      );
+      expect(result.current.productForm.name).toBe('제품2');
+      expect(result.current.productForm.price).toBe(20000);
+      expect(result.current.productForm.stock).toBe(104);
+      expect(result.current.productForm.discounts).toEqual([
+        { quantity: 50, rate: 0.4 },
+      ]);
+    });
+
+    test('useDiscountForm: 할인 추가 form', () => {
+      const { result } = renderHook(() =>
+        useDiscountForm({ quantity: 0, rate: 0 })
+      );
+
+      // 초기값 확인
+      expect(result.current.discountForm).toEqual({ quantity: 0, rate: 0 });
+
+      // quantity 업데이트 확인
+      act(() => result.current.updateDiscountForm('quantity', 60));
+      expect(result.current.discountForm.quantity).toBe(60);
+
+      // rate 업데이트 확인
+      act(() => result.current.updateDiscountForm('rate', 0.5));
+      expect(result.current.discountForm.rate).toBe(0.5);
+
+      // reset 확인
+      act(() => result.current.resetDiscountForm());
+      expect(result.current.discountForm).toEqual({ quantity: 0, rate: 0 });
+    });
+
+    test('useCouponForm: 할인 추가 form', () => {
+      const { result } = renderHook(() =>
+        useCouponForm({
+          name: '',
+          code: '',
+          discountType: 'amount',
+          discountValue: 0,
+        })
+      );
+
+      // 초기값 확인
+      expect(result.current.couponForm).toEqual({
+        name: '',
+        code: '',
+        discountType: 'amount',
+        discountValue: 0,
+      });
+
+      // name 업데이트 확인
+      act(() => result.current.updateCouponForm('name', '쿠폰1'));
+      expect(result.current.couponForm.name).toBe('쿠폰1');
+
+      // code 업데이트 확인
+      act(() => result.current.updateCouponForm('code', '쿠폰코드1'));
+      expect(result.current.couponForm.code).toBe('쿠폰코드1');
+
+      // discountType 업데이트 확인
+      act(() => result.current.updateCouponForm('discountType', 'percentage'));
+      expect(result.current.couponForm.discountType).toBe('percentage');
+
+      // discountValue 업데이트 확인
+      act(() => result.current.updateCouponForm('discountValue', 50));
+      expect(result.current.couponForm.discountValue).toBe(50);
+
+      // reset 확인
+      act(() => result.current.resetCouponForm());
+      expect(result.current.couponForm).toEqual({
+        name: '',
+        code: '',
+        discountType: 'amount',
+        discountValue: 0,
+      });
+    });
+
+    test('useStock: 재고 최신화', () => {
+      const { result } = renderHook(() => useStock(0));
+
+      // 초기값 확인
+      expect(result.current.stock).toEqual(0);
+
+      // 재고 업데이트 확인
+      const mockCartItems = [
+        {
+          product: {
+            id: 'p1',
+            name: '제품1',
+            price: 10000,
+            stock: 48,
+            discounts: [],
+          },
+          quantity: 42,
+        },
+      ];
+      const mockProduct = {
+        id: 'p1',
+        name: '제품1',
+        price: 10000,
+        stock: 48,
+        discounts: [],
+      };
+      act(() => result.current.updateStock(mockCartItems, mockProduct));
+
+      expect(result.current.stock).toEqual(6);
     });
   });
 });
